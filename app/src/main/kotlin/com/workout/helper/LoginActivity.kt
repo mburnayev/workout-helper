523466eed395592355e01498088df02ac674848c
mburnayev <misha@burnayev.com>
2026-01-07 23:48:45 -0500

    client object location consolidation

--- app/src/main/kotlin/com/workout/helper/LoginActivity.kt
+++ app/src/main/kotlin/com/workout/helper/LoginActivity.kt
@@ -7,9 +7,24 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.textfield.TextInputEditText
+import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.gotrue.Auth
 import io.github.jan.supabase.gotrue.auth
 import io.github.jan.supabase.gotrue.providers.builtin.Email
+import io.github.jan.supabase.postgrest.Postgrest
 import kotlinx.coroutines.launch
+
+object SupabaseClient {
+    val client by lazy {
+        createSupabaseClient(
+            supabaseUrl = "https://qunlvbcbdxdbyhzqlzor.supabase.co",
+            supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF1bmx2YmNiZHhkYnloenFsem9yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYzMjA5OTMsImV4cCI6MjA3MTg5Njk5M30.IJcWQU0xe_2DQhFSqHMjehQtI-pOVutj4mCiCC_egd4"
+        ) {
+            install(Auth)
+            install(Postgrest)
+        }
+    }
+}
 
 class LoginActivity : AppCompatActivity() {
     
